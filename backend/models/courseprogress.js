e336af236039bbdbf04837d3985344c141870d75
kalmatheroshan <kalmatheroshan@gmail.com>
2025-04-07 17:36:37 +0530

    modified logic done

--- backend/models/courseprogress.js
+++ backend/models/courseprogress.js
@@ -2,12 +2,18 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const courseProgressSchema = new Schema({
-    course_id: { type: mongoose.Schema.Types.ObjectId, ref: "Course" },
+    course_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course"
+    },
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "user",
     },
-    completedVideos: [{ type: mongoose.Schema.Types.ObjectId, ref: "SubSection" }],
+    completedVideos: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubSection"
+    }],
 })
 
 module.exports = mongoose.model('CourseProgress', courseProgressSchema);
